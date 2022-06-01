8c3b28c82626459f589500ea5bab3eceeb11650e
ariakutty <kuttya25@greenhill.org>
2022-06-01 09:37:56 -0500

    new one

---
+++ addnumbers.py
@@ -0,0 +1,18 @@
+#Aria Kutty 
+#Two Numbers
+# We will ask user for two numbers
+# and add them
+# anything after # is 
+# ignored by the computer
+#import things
+import os
+os.system('clear')
+#Use Variables
+Num1=23
+Num2=12
+answer=Num1+Num2
+#Operators +.-.*,/ 
+print('The addition of', Num1, 'and', Num2, 'is', answer )
+print(Num1) #after each print it makes a return
+print(Num2, end= '') #change end to space
+print(answer)
